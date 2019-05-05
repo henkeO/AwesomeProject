6415ac21846b3cb4dc1b5a7d683b278293a825a1
Henrik Olsen <henrikolsens@gmail.com>
2019-05-05 22:11:32 +0200

    test

--- src/components/WelcomeScreen.js
+++ src/components/WelcomeScreen.js
@@ -5,7 +5,7 @@ class WelcomeScreen extends Component {
   render() {
     return (
       <View>
-        <Text style={{fontSize: 30, textAlign: 'center', fontWeight: 'bold', color: '#FFF'}}>SABRINA & HENRIK'S</Text>
+        <Text style={{fontSize: 30, textAlign: 'center', fontWeight: 'bold', color: '#FFF'}}>Sabrina & Henrik'S</Text>
         <Text style={{fontSize: 20, textAlign: 'center', color: '#FFF'}}>AWESOME PROJECT</Text>
       </View>
     );
