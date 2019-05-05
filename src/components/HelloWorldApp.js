f059592fe2f104bc52edf2c267ac3a9367309884
Sabrina Palm <sabrina_palm@hotmail.com>
2019-05-05 20:32:19 +0200

    changed text

--- src/components/HelloWorldApp.js
+++ src/components/HelloWorldApp.js
@@ -5,7 +5,8 @@ class HelloWorldApp extends Component {
   render() {
     return (
       <View>
-        <Text style={{fontSize: 17, fontWeight: 'bold'}}>SABRINA & HENRIK</Text>
+        <Text style={{fontSize: 20, textAlign: 'center', fontWeight: 'bold'}}>SABRINA & HENRIK'S</Text>
+        <Text style={{fontSize: 14, textAlign: 'center'}}>AWESOME PROJECT</Text>
       </View>
     );
   }
