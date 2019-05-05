1d49d6ba8e98388675feb44eb7ac2c1793c3ab50
Sabrina Palm <sabrina_palm@hotmail.com>
2019-05-05 20:10:11 +0200

    first commit

---
+++ src/components/HelloWorldApp.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+class HelloWorldApp extends Component {
+  render() {
+    return (
+      <View>
+        <Text style={{fontSize: 17, fontWeight: 'bold'}}>SABRINA & HENRIK</Text>
+      </View>
+    );
+  }
+}
+
+export default HelloWorldApp;
